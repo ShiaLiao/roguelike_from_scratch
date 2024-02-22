c36b595c7cf3939861a96e976ad038362cfdbadc
ShiaLiao <xiyanliao2000@gmail.com>
2024-02-22 01:02:12 -0800

    part4

--- actions.py
+++ actions.py
@@ -10,15 +10,15 @@ if TYPE_CHECKING:
 
 class Action:
     def perform(self, engine: Engine, entity: Entity) -> None:
-            """Perform this action with the objects needed to determine its scope.
+        """Perform this action with the objects needed to determine its scope.
 
-            `engine` is the scope this action is being performed in.
+        `engine` is the scope this action is being performed in.
 
-            `entity` is the object performing the action.
+        `entity` is the object performing the action.
 
-            This method must be overridden by Action subclasses.
-            """
-            raise NotImplementedError()
+        This method must be overridden by Action subclasses.
+        """
+        raise NotImplementedError()
 
 
 
--- engine.py
+++ engine.py
@@ -6,13 +6,16 @@ from tcod.console import Console
 from entity import Entity
 from input_handlers import EventHandler
 from game_map import GameMap
+from tcod.map import compute_fov
 
 class Engine:
     def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
         self.entities = entities
         self.event_handler = event_handler
-        self.player = player
         self.game_map = game_map
+        
+        self.player = player
+        self.update_fov()
         
         
     def handle_events(self, events: Iterable[Any]) -> None:
@@ -23,11 +26,24 @@ class Engine:
                 continue
 
             action.perform(self, self.player)
+            self.update_fov()  # Update the FOV before the players next action.
 
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
+        
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            # Only print entities that are in the FOV
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
